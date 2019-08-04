70d182d179910d66e98cddc4bf1d303ee31a9fb8
James Woodley <james@lightestnight.com>
2019-08-04 21:09:37 +0100

    Fix up connection details

--- Caching.Redis.Tests/TagCache/Helpers/ConnectionHelper.cs
+++ Caching.Redis.Tests/TagCache/Helpers/ConnectionHelper.cs
@@ -4,13 +4,9 @@ namespace LightestNight.System.Caching.Redis.Tests.TagCache.Helpers
 {
     public static class ConnectionHelper
     {
-//        public static string IntegrationTestHost => Environment.GetEnvironmentVariable("INTEGRATION_TEST_HOST");
-//        public static string Password => Environment.GetEnvironmentVariable("INTEGRATION_TEST_PASSWORD");
-//        public static int Port => Convert.ToInt32(Environment.GetEnvironmentVariable("INTEGRATION_TEST_PORT"));
-//        public static bool UseSsl => Convert.ToBoolean(Environment.GetEnvironmentVariable("INTEGRATION_TEST_USE_SSL"));
-        public static string IntegrationTestHost => "integration-tests-lightestnight-71a6.aivencloud.com";
-        public static string Password =>  "ngf1fkibv9fh5cv7";
-        public static int Port => 19198;
-        public static bool UseSsl => true;
+        public static string IntegrationTestHost => Environment.GetEnvironmentVariable("INTEGRATION_TEST_HOST");
+        public static string Password => Environment.GetEnvironmentVariable("INTEGRATION_TEST_PASSWORD");
+        public static int Port => Convert.ToInt32(Environment.GetEnvironmentVariable("INTEGRATION_TEST_PORT"));
+        public static bool UseSsl => Convert.ToBoolean(Environment.GetEnvironmentVariable("INTEGRATION_TEST_USE_SSL"));
     }
 }
